c5fc848dccf026288db3409d97ae92993cfede15
SidharthMahai <sidharth.mahai.work@gmail.com>
2021-06-26 23:03:44 +0530

    Add Social Links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myportfolio';
+  title = 'Sidharth Mahai - Portfolio';
+
+  openGitHub() {
+    window.open("https://github.com/SidharthMahai/");
+  }
+
+  
+  openFacebook() {
+    window.open("https://www.facebook.com/sidharth.sharma.52035/");
+  }
+
+  
+  openLinkedIn() {
+    window.open("https://www.linkedin.com/in/sidharth-mahai-52a805173/");
+  }
+
 }
